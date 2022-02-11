51dc78f474a1589f78841bf55b9291a9e494583d
emileeostendorf <99349406+emileeostendorf@users.noreply.github.com>
2022-02-11 11:35:03 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -43,3 +43,4 @@ let Wheel = sprites.create(img`
     ................................
     `, SpriteKind.Player)
 game.splash("Hello " + game.askForString("What is your name?") + "! Welcome to the game!")
+let myCounter = sevenseg.createCounter(SegmentStyle.Medium, SegmentScale.Half, 3)
